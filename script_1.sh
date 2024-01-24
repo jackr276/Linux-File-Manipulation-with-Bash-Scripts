d2b18d84ed0578d489e81e776cd498f9f2748580
jackr276 <jack_m_robbins@me.com>
2024-01-24 14:40:04 -0500

    created ability to change directories

--- script_1.sh
+++ script_1.sh
@@ -1,16 +1,25 @@
 #Jack Robbins
 #Homework Assignment 1, Problem 1
 
+NEWLINE=$'\n'
+
 #prompt user
 read -p "Enter a directory that you would like to move to: " dir
 
 #verify that directory exists, if it does not exit
 if [ ! -d $dir ]; then
-	echo "Invalid Entry, no directory named $dir \n"
+	echo "Invalid Entry, no directory named ${dir} ${NEWLINE}"
 	exit 0
 fi
 
-
 #move into the directory, now that we know its validity
+cd ./$dir
+
+echo "All files in ${dir} ${NEWLINE} "
+#list all the files
+ls -1
+echo "${NEWLINE}"
+
+read -p "Please select a file by name: " fname
 
 

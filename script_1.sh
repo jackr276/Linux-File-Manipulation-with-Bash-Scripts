0d1e80c21b19e3843dc3c7ce8588279e5e51eca5
jackr276 <jack_m_robbins@me.com>
2024-01-24 15:57:15 -0500

    script_1 now working

--- script_1.sh
+++ script_1.sh
@@ -1,6 +1,8 @@
 #Jack Robbins
 #Homework Assignment 1, Problem 1
 
+#!bin/bash
+
 NEWLINE=$'\n'
 
 #prompt user
@@ -8,8 +10,8 @@ read -p "Enter a directory that you would like to move to: " dir
 
 #verify that directory exists, if it does not exit
 if [ ! -d $dir ]; then
-	echo "Invalid Entry, no directory named ${dir} ${NEWLINE}"
-	exit 0
+    echo "Invalid Entry, no directory named ${dir} ${NEWLINE}"
+    exit 0
 fi
 
 #move into the directory, now that we know its validity
@@ -20,6 +22,40 @@ echo "All files in ${dir} ${NEWLINE} "
 ls -1
 echo "${NEWLINE}"
 
-read -p "Please select a file by name: " fname
+read -p "Please select a file: " fname
 
+#So long as the file choice is bad, keep prompting the user
+while [ ! -f "$fname" ]
+do
+    echo "File name is invalid, try again"
+    read -p "Please select a file: " fname
+done
 
+#get the number of lines
+lines=$(cat $fname | wc -l)
+
+#compute the initial first and last bounds
+last=$lines
+first=$(($lines-9))
+
+wantsDisp=$'yes'
+
+while [ "$wantsDisp" == yes ]
+do
+   #If we hit the beginning of the file, leave the loop
+   if [ $first -lt 0 ]; then
+      head -$last "$fname" | tail +0
+      echo "Beginning of file reached"
+      break
+   fi
+
+   #print out ten lines
+   head -$last "$fname" | tail +$first
+
+   #update the lines
+   last=$((last-10))
+   first=$((first-10))
+
+   #prompt user
+   read -p "Would you like to diplay 10 more lines(yes/no): " wantsDisp
+done

6593f8325e4d4526d2b8f4710fa0698902352be8
jackr276 <jack_m_robbins@me.com>
2024-02-02 20:29:46 -0500

    script1 can now handle directories with spaces in names

--- script_1.sh
+++ script_1.sh
@@ -9,13 +9,13 @@ NEWLINE=$'\n'
 read -p "Enter a directory that you would like to move to: " dir
 
 #verify that directory exists, if it does not exit
-if [ ! -d $dir ]; then
+if [ ! -d "$dir" ]; then
     echo "Invalid Entry, no directory named ${dir} ${NEWLINE}"
     exit 0
 fi
 
 #move into the directory, now that we know its validity
-cd $dir
+cd "$dir"
 
 echo "All files in ${dir} ${NEWLINE} "
 #list all files only
